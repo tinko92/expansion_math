8ef079843429d0706b1ad1d7d05503a2bbf1a9b7
Tinko Bartels <mail@tinkobartels.de>
2020-03-18 00:40:36 +0100

    added output.

--- demo.cpp
+++ demo.cpp
@@ -4,21 +4,25 @@
 
 int main() {
 	using A = expansion<double>;
-	auto R = A(1e100) + A(1e-20) + A(1e-20) + A(1e-40) - A(1e100) - A(2.0) * A(1e-20);
-	std::cout << R.estimate() << "\n"; //completely unaffected by loss of significance
-	
+	auto x = A(1e100) + A(1e-20) + A(1e-20) + A(1e-40) - A(1e100) - A(2.0) * A(1e-20);
+	std::cout << x.estimate() << "\n"; 
+	//output: 1e-40
+	//completely unaffected by loss of significance
 
 
 	std::cout.precision(15);
 	auto start = std::chrono::system_clock::now();
-	A f(0.0);
+	A s(0.0);
 	for(double i = 1.0; i < 2e7; i += 1.0)
-		f = f + A(1.0/(i * i));
+		s = s + A(1.0/(i * i));
 	auto end = std::chrono::system_clock::now();
 	auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-	std::cout << "elapsed time: " << elapsed.count() << "ms\n"; //acceptable speed
-	std::cout << "result: " << f.estimate() << "\n";	    //precise results
-	
+	std::cout << "elapsed time: " << elapsed.count() << "ms\n";
+	//output: elapsed time: 698ms
+	//acceptable speed
+	std::cout << "result: " << s.estimate() << "\n";
+	//result: 1.64493401684823
+	//precise results
 
 
 	auto det1 = (A(1e-20) - A(1e20)) *
@@ -36,6 +40,10 @@ int main() {
 	std::cout << (det1.estimate() > 0 ? 1 : (det1.estimate() == 0 ? 0 : -1)) << "\n"
 		  << (det2.estimate() > 0 ? 1 : (det2.estimate() == 0 ? 0 : -1)) << "\n"
 		  << (det3.estimate() > 0 ? 1 : (det3.estimate() == 0 ? 0 : -1)) << "\n";
+	//output:
+	//1
+	//0
+	//-1
 	//suitable to build robust geometric predicates.
 	return 0;
 }
